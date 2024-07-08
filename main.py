59b50162bd3871f6f7f1f49f31d8947e1969080d
Josh Brown <jbrown1618@github.com>
2024-07-08 16:17:53 -0400

    Do not dump json into the markdown files

--- main.py
+++ main.py
@@ -11,35 +11,74 @@ def main():
     with open(input_path) as f:
         spec = json.load(f)
 
-    make_info_file(spec, output_path)
     for path, path_spec in spec["paths"].items():
         for method, method_spec in path_spec.items():
             make_operation_file(path, method, method_spec, spec, output_path)
-
-
-def make_info_file(spec: dict, output_path: str):
-    info = spec["info"]
-    info_file = Path(os.path.join(output_path, "README.md"))
-    info_file.parent.mkdir(exist_ok=True, parents=True)
-    info_file.write_text(f"# {info['title']}\n\n{info['description']}\n\n```json\n{json.dumps(info, indent=4)}\n```")
 
 
 def make_operation_file(path: str, method: str, method_spec: dict, spec: dict, output_path: str):
     if method != "get":
         return # We're only interested in read operations
     
-    out = f"# {method_spec['summary']}\n\n`{method} {path}`\n\n{method_spec['description']}\n\n## Operation Object\n\n```json\n{json.dumps(method_spec, indent=4)}\n```"
-    
-    refs = get_references(method_spec, spec)
-    if len(refs) > 0:
-        out += "\n\n## References"
-        for key, ref in refs:
-            out += f"\n\n### `{key}`\n\n```json\n{json.dumps(ref, indent=4)}\n```"
-
+    out = [
+        f"# {method_spec['summary']}",
+        "",
+        f"`{method.upper()} {path}`",
+        "",
+        f"{method_spec['description']}",
+        "",
+        f'[{method_spec["externalDocs"]["description"]}]({method_spec["externalDocs"]["url"]})',
+        "",
+    ] + params_section(method_spec, spec)
     
     method_file = Path(os.path.join(output_path, path[1:], f"{method}.md"))
     method_file.parent.mkdir(exist_ok=True, parents=True)
-    method_file.write_text(out)
+    method_file.write_text("\n".join(out))
+
+
+def params_section(method_spec: dict, spec: dict):
+    if "parameters" not in method_spec:
+        return []
+    
+    path_list_items = []
+    query_list_items = []
+
+    for parameter in method_spec["parameters"]:
+        if "$ref" in parameter:
+            parameter = get_reference(parameter["$ref"], spec)
+
+        param_type = ""
+        if "schema" in parameter and "type" in parameter["schema"]:
+            param_type = parameter["schema"]["type"]
+
+        if 'required' in parameter and parameter['required']:
+            param_type += ", required"
+
+        list_item = f"- `{parameter['name']}`"
+
+        if param_type:
+            list_item += f" ({param_type})"
+
+        if "description" in parameter:
+            list_item += ": " + parameter["description"]
+
+        if "in" in parameter and parameter["in"] == "path":
+            path_list_items.append(list_item)
+        else:
+            query_list_items.append(list_item)
+
+    out = [
+        f"## All Parameters for \"{method_spec['summary']}\"", 
+        ""
+    ]
+
+    if len(path_list_items) > 0:
+        out += ["### Path Parameters", ""] + path_list_items
+
+    if len(query_list_items) > 0:
+        out += ["### Query Parameters", ""] + query_list_items
+
+    return out
 
 
 def get_references(method_spec: dict, spec: dict):
